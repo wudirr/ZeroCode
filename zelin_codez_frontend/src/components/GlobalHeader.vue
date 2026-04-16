835603bee357b71aef74411eb61964e4d5639064
吴迪 <14924823+wudijkl@user.noreply.gitee.com>
2026-04-16 14:20:25 +0800

    feat:完成前端的初始化

--- zelin_codez_frontend/src/components/GlobalHeader.vue
+++ zelin_codez_frontend/src/components/GlobalHeader.vue
@@ -22,7 +22,35 @@
       <!-- 右侧：用户操作区域 -->
       <a-col>
         <div class="user-login-status">
-          <a-button type="primary">登录</a-button>
+          <div v-if="userLoginStore.loginUser.id">
+            <a-space>
+              <a-dropdown placement="bottom" arrow>
+                <a-avatar :src="(userLoginStore.loginUser as API.UserQueryVO).userAvatar" size="large" />
+                <template #overlay>
+                  <a-menu>
+                    <a-menu-item @click="toPersonalCenter">
+                      个人中心
+                      <UserOutlined />
+                    </a-menu-item>
+                    <a-menu-item @click="toSettings">
+                      其他设置
+                      <UserOutlined />
+                    </a-menu-item>
+                    <a-menu-item @click="logout">
+                      登出
+                      <LogoutOutlined />
+                    </a-menu-item>
+                  </a-menu>
+                </template>
+              </a-dropdown>
+              <div style="font-size: 17px; color: #00c2ff">
+                {{ userLoginStore.loginUser.userName ?? '未登录' }}
+              </div>
+            </a-space>
+          </div>
+          <div v-else>
+            <a-button type="primary" href="/user/login">登录</a-button>
+          </div>
         </div>
       </a-col>
     </a-row>
@@ -32,7 +60,10 @@
 <script setup lang="ts">
 import { h, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import type { MenuProps } from 'ant-design-vue'
+import { message, type MenuProps } from 'ant-design-vue'
+import { useUserLoginStore } from '@/stores/UserLoginStore'
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons-vue'
+import { userLogout } from '@/api/userController'
 
 const router = useRouter()
 // 当前选中菜单
@@ -41,7 +72,9 @@ const selectedKeys = ref<string[]>(['/'])
 router.afterEach((to, from, next) => {
   selectedKeys.value = [to.path]
 })
-
+//引入全局登录登录状态
+const userLoginStore = useUserLoginStore()
+userLoginStore.fetchLoginUser()
 // 菜单配置项
 const menuItems = ref([
   {
@@ -53,6 +86,11 @@ const menuItems = ref([
     key: '/about',
     label: '关于',
     title: '关于我们',
+  },
+  {
+    key: '/admin/user/manage',
+    label: '用户管理',
+    title: '用户管理',
   },
   {
     key: 'others',
@@ -68,6 +106,28 @@ const handleMenuClick: MenuProps['onClick'] = (e) => {
   // 跳转到对应页面
   if (key.startsWith('/')) {
     router.push(key)
+  }
+}
+//跳转到个人中心
+const toPersonalCenter = () => {
+  message.warning('个人中心功能正在开发中，敬请期待！')
+}
+//跳转到设置页面
+const toSettings = () => {
+  message.warning('设置功能正在开发中，敬请期待！')
+}
+//登出功能
+const logout = async () => {
+  const res = await userLogout()
+  if (res.code === 200 && res.data) {
+    userLoginStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('登出成功')
+    router.push({
+      path: '/user/login',
+      replace: true,
+    })
   }
 }
 </script>
--- zelin_codez_frontend/src/views/HomeView.vue
+++ zelin_codez_frontend/src/views/HomeView.vue
@@ -3,7 +3,6 @@
     这个是主页
     <a-divider></a-divider>
     <a-space>
-      <a-button type="primary" @click="apiTest">朴素按钮</a-button>
       <a-button danger>危险按钮</a-button>
       <a-button ghost>幽灵按钮</a-button>
     </a-space>
@@ -18,13 +17,7 @@
 </template>
 
 <script setup lang="ts">
-import { hello } from '@/api/helloController'
 import { ref, reactive, onMounted } from 'vue'
-const apiTest = () => {
-  hello().then((res) => {
-    console.log(res);
-  })
-}
 // 生命周期
 onMounted(() => {
   console.log('组件已挂载')
@@ -33,6 +26,5 @@ onMounted(() => {
 
 <style scoped>
 #HomeViewHome-View {
-
 }
 </style>
