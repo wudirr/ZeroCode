e04230947f0a54ebe9573c62626ffb487fcae94b
wudirr <2270572360@qq.com>
2026-04-18 19:48:04 +0800

    style:格式化代码

--- zelin_codez_frontend/src/stores/UserLoginStore.ts
+++ zelin_codez_frontend/src/stores/UserLoginStore.ts
@@ -1,28 +1,28 @@
-import { getLoginUser } from "@/api/userController";
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import { getLoginUser } from '@/api/userController'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useUserLoginStore = defineStore('userLogin',() => {
-    const loginUser = ref<API.UserLoginVO>({
-        userName: '未登录'
-    })
+export const useUserLoginStore = defineStore('userLogin', () => {
+  const loginUser = ref<API.UserLoginVO>({
+    userName: '未登录',
+  })
 
-    //获取登录用户信息
-    async function fetchLoginUser(){
-        const res = await getLoginUser();
-        if(res.code === 200 && res.data){
-            loginUser.value = res.data;
-        }
+  //获取登录用户信息
+  async function fetchLoginUser() {
+    const res = await getLoginUser()
+    if (res.code === 200 && res.data) {
+      loginUser.value = res.data
     }
+  }
 
-    //设置登录用户信息
-    async function setLoginUser(newLoginUser: any){
-        loginUser.value = newLoginUser;
-    }
+  //设置登录用户信息
+  async function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
+  }
 
-    return {
-        loginUser,
-        fetchLoginUser,
-        setLoginUser
-    }
+  return {
+    loginUser,
+    fetchLoginUser,
+    setLoginUser,
+  }
 })
