0f66ce40c4daea77ae31b3e693400337bac3b4df
wudirr <2270572360@qq.com>
2026-04-18 19:50:29 +0800

    feat:完善用户管理界面,增加分页和删除功能

--- zelin_codez_frontend/src/components/UserManagerPage.vue
+++ zelin_codez_frontend/src/components/UserManagerPage.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="UserManagePage">
-    <a-table :columns="columns" :data-source="data">
+    <a-table
+      :columns="columns"
+      :data-source="data"
+      :pagination="pagination"
+      @change="handleTableChange"
+    >
       <template #headerCell="{ column }">
         <template v-if="column.key === 'id'">
           <span>
@@ -16,6 +21,11 @@
             {{ record.userName }}
           </a>
         </template>
+        <template v-else-if="column.key === 'userAvatar'">
+          <span>
+            <a-image :width="42" :src="record.userAvatar" />
+          </span>
+        </template>
         <template v-else-if="column.key === 'userRole'">
           <span>
             <a-tag
@@ -30,7 +40,7 @@
         </template>
         <template v-else-if="column.key === 'action'">
           <span>
-            <a-button type="primary" ghost>编辑</a-button>
+            <a-button danger @click="confirm(record.id)">删除</a-button>
           </span>
         </template>
       </template>
@@ -38,11 +48,11 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { list } from '@/api/userController'
-import { SmileOutlined } from '@ant-design/icons-vue'
-import { message } from 'ant-design-vue'
-import { ref } from 'vue'
+import { page, remove } from '@/api/userController'
+import { message, Modal } from 'ant-design-vue'
+import { computed, createVNode, ref } from 'vue'
 import { onMounted } from 'vue'
+import { ExclamationCircleOutlined } from '@ant-design/icons-vue'
 const columns = [
   {
     name: 'ID',
@@ -54,6 +64,12 @@ const columns = [
     title: '用户名',
     dataIndex: 'userName',
     key: 'userName',
+    align: 'center',
+  },
+  {
+    title: '头像',
+    dataIndex: 'userAvatar',
+    key: 'userAvatar',
     align: 'center',
   },
   {
@@ -76,16 +92,76 @@ const columns = [
 ]
 
 const data = ref([{}])
-onMounted(async () => {
-  const res = await list()
-  if (res.code === 200 && res.data) {
-    res.data.forEach((item) => {
+
+const deleteId = ref<string>()
+
+const total = ref(0)
+const searchParams = ref<API.UserQueryRequest>({
+  pageNum: 1,
+  pageSize: 10,
+})
+
+const pagination = computed(() => {
+  return {
+    current: searchParams.value.pageNum ?? 1,
+    pageSize: searchParams.value.pageSize ?? 10,
+    total: total.value,
+    showSizeChanger: true,
+    showTotal: (total: number) => `共${total}条`,
+  }
+})
+
+const handleTableChange = (pagination: any) => {
+  searchParams.value.pageNum = pagination.current
+  searchParams.value.pageSize = pagination.pageSize
+  fetchData()
+}
+async function fetchData() {
+  const res = await page({
+    ...(searchParams.value as any),
+  })
+  if (res.code === 200 && res.data?.records) {
+    res.data.records.forEach((item) => {
       item.userRole = item.userRole?.split(',') as any // 将字符串转换为数组
     })
-    data.value = res.data as any[]
-    console.log(data.value)
+    data.value = res.data.records as any[]
+    total.value = res.data.totalRow ?? 0
+    console.log(res.data)
   } else {
     message.error(res.message || '获取用户列表失败')
   }
+}
+
+const handlerDelete = async () => {
+  const id = deleteId.value
+  const res = await remove({ id })
+  if (res.code === 200 && res.data) {
+    message.success('删除成功')
+    fetchData()
+  } else {
+    message.error(res.message || '删除失败')
+  }
+  console.log('aaa')
+}
+
+const confirm = (id: string) => {
+  deleteId.value = id
+  Modal.confirm({
+    title: '警告',
+    icon: createVNode(ExclamationCircleOutlined),
+    content: '您确定要删除该用户吗？',
+    okText: '确认',
+    cancelText: '取消',
+    onOk: () => handlerDelete(),
+  })
+}
+onMounted(() => {
+  fetchData()
 })
 </script>
+
+<style scoped>
+#UserManagePage {
+  min-width: 768px;
+}
+</style>
