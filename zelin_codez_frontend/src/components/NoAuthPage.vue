906676f8231ada311d22cb2464c08f29e26f3db3
wudirr <2270572360@qq.com>
2026-04-18 19:50:05 +0800

    feat:添加无权限页面以及路由

---
+++ zelin_codez_frontend/src/components/NoAuthPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-result status="403" title="403" sub-title="你没有权限访问此页面">
+    <template #extra>
+      <a-button type="primary" @click="toHomeView">首页</a-button>
+    </template>
+  </a-result>
+</template>
+<script>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const toHomeView = () => {
+  router.push('/')
+}
+</script>
