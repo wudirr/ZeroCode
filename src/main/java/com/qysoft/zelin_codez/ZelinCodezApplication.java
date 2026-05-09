441fe435b7c6e242a5933d645d6a1c127b8b70e1
wudirr <2270572360@qq.com>
2026-05-10 00:42:42 +0800

    feat:新增MapperScan扫描mapper类

--- src/main/java/com/qysoft/zelin_codez/ZelinCodezApplication.java
+++ src/main/java/com/qysoft/zelin_codez/ZelinCodezApplication.java
@@ -1,11 +1,13 @@
 package com.qysoft.zelin_codez;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.qysoft.zelin_codez.mapper")
 public class ZelinCodezApplication {
 
     public static void main(String[] args) {
