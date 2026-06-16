b5fd731be1e5a5dd050800afd3b0822a7964b8f4
wudirr <2270572360@qq.com>
2026-06-16 18:38:47 +0800

    feat:使用Langchain4j提供的InputGuardrail[输入护轨机制]实现了对输入提示词的安全审查

--- src/main/java/com/qysoft/zelin_codez/ai/guardrails/PromptSafetyInputGuardrail.java
+++ src/main/java/com/qysoft/zelin_codez/ai/guardrails/PromptSafetyInputGuardrail.java
@@ -1,7 +1,6 @@
 package com.qysoft.zelin_codez.ai.guardrails;
 
 import com.qysoft.zelin_codez.common.constant.GlobalConstant;
-import com.qysoft.zelin_codez.exception.BusinessException;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.guardrail.InputGuardrail;
 import dev.langchain4j.guardrail.InputGuardrailResult;
@@ -41,7 +40,7 @@ public class PromptSafetyInputGuardrail implements InputGuardrail {
     public InputGuardrailResult validate(UserMessage userMessage) {
         String input = userMessage.singleText();
         if (input.length() > 1000) {
-            return fatal(GlobalConstant.INPUT_TOO_LONG, new BusinessException(GlobalConstant.INPUT_TOO_LONG));
+            return fatal(GlobalConstant.INPUT_TOO_LONG);
         }
         //检查内容
         if (input.trim().isEmpty()) {
@@ -49,12 +48,12 @@ public class PromptSafetyInputGuardrail implements InputGuardrail {
         }
         //检查是否存在敏感词
         if (SENSITIVE_WORDS.contains(input.trim().toLowerCase())) {
-            return fatal(GlobalConstant.INPUT_EXCEPTION, new BusinessException(GlobalConstant.INPUT_EXCEPTION));
+            return fatal(GlobalConstant.INPUT_EXCEPTION);
         }
         //检查是否存在注入攻击
         for (Pattern pattern : INJECTION_PATTERNS) {
             if (pattern.matcher(input).find()) {
-                return fatal(GlobalConstant.INPUT_EXCEPTION, new BusinessException(GlobalConstant.INPUT_EXCEPTION));
+                return fatal(GlobalConstant.INPUT_EXCEPTION);
             }
         }
         return success();
