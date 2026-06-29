8f97b76dd7201a3f3872f18f39216a8fff07e02d
wudirr <2270572360@qq.com>
2026-06-29 22:22:05 +0800

    feat:排除RedisEmbeddingStoreAutoConfiguration这个自动装配类

--- src/main/java/com/qysoft/zelin_codez/ZelinCodezApplication.java
+++ src/main/java/com/qysoft/zelin_codez/ZelinCodezApplication.java
@@ -1,12 +1,13 @@
 package com.qysoft.zelin_codez;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisEmbeddingStoreAutoConfiguration.class)
 @EnableAspectJAutoProxy(exposeProxy = true)
 @EnableCaching
 @MapperScan("com.qysoft.zelin_codez.mapper")
